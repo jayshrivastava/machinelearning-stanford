1ae488b15dc8b015be7370f2cd8dd1d306186f0f
unknown <jayshrivastava15@gmail.com>
2018-02-04 12:56:41 -0500

    finished gradient descent for one var using vectorization

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,17 +16,17 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
+    
+    temp = theta(1,1) - alpha*ones(1,m)*(X*theta - y)/m;
+    temp2 = theta(2,1) - alpha*X(:,2)'*(X*theta - y)/m;
+    theta(1,1) = temp;
+    theta(2,1) = temp2; 
 
 
     % ============================================================
 
     % Save the cost J in every iteration    
-    % J_history(iter) = computeCost(X, y, theta);
+    J_history(iter) = computeCost(X, y, theta);
 
 end
 
